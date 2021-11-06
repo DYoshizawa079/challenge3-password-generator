3b87484881ae37c1625e7470b64faa1ae3bbda78
Skapa804 <daniel@Daniels-MacBook-Pro-3.local>
2021-11-06 00:33:29 -0400

    Initial test version

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
 // Assignment code here
 
+let password = {
+  numOfChar: 0,
+  lowercase: false,
+  uppercase: false,
+  numbers: false,
+  special: false
+};
+
+// Prompt for password parameters wanted
+var setPasswordLength = function() {
+  password.numOfChar = prompt("How long do you want the password to be (in characters)? Enter a number between 8 and 128.");
+  password.numOfChar = parseInt(password.numOfChar);
+  console.log("password.numOfChar" + password.numOfChar);
+}
+
+var promptCharacters = function() {
+  charLowercase = prompt("Do you want the password to include lowercase characters? Enter 'yes' if you do.");
+  charLowercase = charLowercase.toLowerCase();
+  if (charLowercase === 'yes') {
+    password.lowercase = true;
+  }
+  charUppercase = prompt("Do you want the password to include uppercase characters? Enter 'yes' if you do.");
+  charUppercase = charUppercase.toLowerCase();
+  if (charUppercase === 'yes') {
+    password.uppercase = true;
+  }
+  console.log(password);
+}
+
+// Generate the password based on criteria and number of characters requested
+var generatePassword = function() {
+
+  setPasswordLength();
+  promptCharacters();
+
+  var result = '';
+
+  var lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+  var lowercaseCharactersLength = lowercaseCharacters.length;
+
+  var uppercaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var uppercaseCharactersLength = uppercaseCharacters.length;
+
+  var counter = 0;
+  while (counter < password.numOfChar) {
+    if(password.lowercase === true) {
+      result += lowercaseCharacters.charAt(Math.floor(Math.random() * lowercaseCharactersLength));
+      counter++;
+    }
+    if(password.uppercase === true) {
+      result += uppercaseCharacters.charAt(Math.floor(Math.random() * uppercaseCharactersLength));
+      counter++;
+    }
+  }
+  return result;
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -8,7 +65,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
