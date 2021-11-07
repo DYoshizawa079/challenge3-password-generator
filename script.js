d1b11be43f1fa3bf6382a6aa2684f829b22fa727
Skapa804 <daniel@Daniels-MacBook-Pro-3.local>
2021-11-06 22:48:14 -0400

    Updates. Note that password has tendency to not reset upon new session

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
 // Assignment code here
 
-let password = {
+var passwordObj = {
   numOfChar: 0,
   lowercase: false,
   uppercase: false,
   numbers: false,
-  special: false
+  special: false,
+  password: ''
 };
+var cancel;
 
 // Prompt for password parameters wanted
 
+// Check for cancellations
+var checkCancellation = function(check) {
+  if (check === null) {
+    cancel = true;
+    alert("You have cancelled the operation. Goodbye!");
+    console.log('cancel ' + cancel);
+  }
+}
+
 // Set the number of characters
 var setPasswordLength = function() {
-  password.numOfChar = prompt("How long do you want the password to be (in characters)? Enter a number between 8 and 128.");
-  password.numOfChar = parseInt(password.numOfChar);
-
-  // Verify that a number between 8 and 128 has been entered
-  if (password.numOfChar < 8 || password.numOfChar > 128 || isNaN(password.numOfChar)) {
+  passwordObj.numOfChar = prompt("How long do you want the password to be (in characters)? Enter a number between 8 and 128.");
+  console.log("passwordObj.numOfChar " + passwordObj.numOfChar);
+  if (passwordObj.numOfChar === NaN || passwordObj.numOfChar === null) {
+    alert("You have cancelled the operation. Goodbye!");
+    cancel = true;
+    return;
+  }
+  passwordObj.numOfChar = parseInt(passwordObj.numOfChar);
+  if (passwordObj.numOfChar < 8 || passwordObj.numOfChar > 128 || isNaN(passwordObj.numOfChar)) {
     alert("Please enter a number between 8 and 128.");
     setPasswordLength();
   }
-  console.log("password.numOfChar " + password.numOfChar);
+  
 }
 
 // Prompt what kind of characters are wanted
 var promptLowercase = function() {
-  charLowercase = prompt("Do you want the password to include lowercase characters? Enter 'yes' if you do. Otherwise leave it blank.");
-  charLowercase = charLowercase.toLowerCase();
+  var charLowercase = prompt("Do you want the password to include lowercase characters? Enter 'yes' if you do. Otherwise leave it blank.");
+  
+  checkCancellation(charLowercase);
+  if(cancel) {
+    return;
+  }
+
   if (charLowercase === 'yes') {
-    password.lowercase = true;
+    passwordObj.lowercase = true;
   } else if (charLowercase === '') {
     // Do nothing
   } else {
@@ -38,10 +58,15 @@ var promptLowercase = function() {
 }
 
 var promptUppercase = function() {
-  charUppercase = prompt("Do you want the password to include uppercase characters? Enter 'yes' if you do. Otherwise leave it blank.");
+  var charUppercase = prompt("Do you want the password to include uppercase characters? Enter 'yes' if you do. Otherwise leave it blank.");
+  
+  checkCancellation(charUppercase);
+  if(cancel) {
+    return;
+  }
   charUppercase = charUppercase.toLowerCase();
   if (charUppercase === 'yes') {
-    password.uppercase = true;
+    passwordObj.uppercase = true;
   } else if (charUppercase === '') {
     // Do nothing
   } else {
@@ -51,10 +76,15 @@ var promptUppercase = function() {
 }
 
 var promptNum = function() {
-  charNum = prompt("Do you want the password to include number characters? Enter 'yes' if you do. Otherwise leave it blank.");
+  var charNum = prompt("Do you want the password to include number characters? Enter 'yes' if you do. Otherwise leave it blank.");
+  
+  checkCancellation(charNum);
+  if(cancel) {
+    return;
+  }
   charNum = charNum.toLowerCase();
   if (charNum === 'yes') {
-    password.numbers = true;
+    passwordObj.numbers = true;
   } else if (charNum === '') {
     // Do nothing
   } else {
@@ -64,10 +94,15 @@ var promptNum = function() {
 }
 
 var promptSpecial = function() {
-  charSpecial = prompt("Do you want the password to include special characters? Enter 'yes' if you do. Otherwise leave it blank.");
+  var charSpecial = prompt("Do you want the password to include special characters? Enter 'yes' if you do. Otherwise leave it blank.");
+  
+  checkCancellation(charSpecial);
+  if(cancel) {
+    return;
+  }
   charSpecial = charSpecial.toLowerCase();
   if (charSpecial === 'yes') {
-    password.special = true;
+    passwordObj.special = true;
   } else if (charSpecial === '') {
     // Do nothing
   } else {
@@ -76,59 +111,81 @@ var promptSpecial = function() {
   }
 }
 
+// Prompt for the types of characters required
+var promptCharTypes = function() {
+  promptLowercase();
+  if(cancel) {
+    return;
+  }
+  promptUppercase();
+  if(cancel) {
+    return;
+  }
+  promptNum();
+  if(cancel) {
+    return;
+  }
+  promptSpecial();
+  if(cancel) {
+    return;
+  }
+
+  // Check whether at least one character type has been selected
+  if (passwordObj.lowercase === false && passwordObj.uppercase === false && passwordObj.numbers === false && passwordObj.special === false ) {
+    alert("Please pick at least one password criteria.");
+    promptCharTypes();
+  }
+}
+
 // Generate the password based on criteria and number of characters requested
 var generatePassword = function() {
 
+  // Reset the password display to blank
+  var passwordDisplay = document.querySelector("#password");
+  passwordDisplay.value = 'test';
+  console.log('passwordDisplay.value ' + passwordDisplay.value);
+
   setPasswordLength();
-  promptLowercase();
-  promptUppercase();
-  promptNum();
-  promptSpecial();
-
-  if (password.lowercase === false && password.uppercase === false) {
-    alert("Please pick at least one password criteria.");
-    promptLowercase();
-    promptUppercase();
-    promptNum();
-    promptSpecial();
+  if(cancel) {
+    cancel = false;
+    return '';
+  }
+  
+  promptCharTypes();
+  if(cancel) {
+    cancel = false;
+    return '';
   }
 
-  console.log(password);
+  console.log(passwordObj);
 
-  var result = '';
+  // The various characters that are available to generate the password
+  var lowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var uppercaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var numChar = [0,1,2,3,4,5,6,7,8,9];
+  var specChar = ['\'','\"','!','#','$','%','&','(',')','+',',','-','*','.','/',':',';','<','>','=','?','\\','@','[',']','^','_','`','{','}','~','|'];
 
-  var lowercaseChar = 'abcdefghijklmnopqrstuvwxyz';
-  var lowercaseCharLength = lowercaseChar.length;
-
-  var uppercaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var uppercaseCharLength = uppercaseChar.length;
-
-  var numChar = '0123456789';
-  var numCharLength = numChar.length;
-
-  var specChar = '!#$%&()*+,-./:;<>=?@[]\^_{}|~';
-  var specCharLength = specChar.length;
-
+  // Pick random charactrs from each set of characters requested. Then join them together to form the password.
   var counter = 0;
-  while (counter < password.numOfChar) {
-    if(password.lowercase === true) {
-      result += lowercaseChar.charAt(Math.floor(Math.random() * lowercaseCharLength));
+  while (counter < passwordObj.numOfChar) {
+    if(passwordObj.lowercase === true) {
+      passwordObj.password += lowercaseChar[Math.floor(Math.random() * lowercaseChar.length)];
       counter++;
     }
-    if(password.uppercase === true) {
-      result += uppercaseChar.charAt(Math.floor(Math.random() * uppercaseCharLength));
+    if(passwordObj.uppercase === true) {
+      passwordObj.password += uppercaseChar[Math.floor(Math.random() * uppercaseChar.length)];
       counter++;
     }
-    if(password.numbers === true) {
-      result += numChar.charAt(Math.floor(Math.random() * numCharLength));
+    if(passwordObj.numbers === true) {
+      passwordObj.password += numChar[Math.floor(Math.random() * numChar.length)];
       counter++;
     }
-    if(password.special === true) {
-      result += specChar.charAt(Math.floor(Math.random() * specCharLength));
+    if(passwordObj.special === true) {
+      passwordObj.password += specChar[Math.floor(Math.random() * specChar.length)];
       counter++;
     }
   }
-  return result;
+  return passwordObj.password;
 
 }
 
